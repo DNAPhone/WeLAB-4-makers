fbf3b3c1537f51a7f0fcd3329928ccce5b2a1da6
Alessandro Grazioli <ale@MacBook-Pro-di-Alessandro-2.local>
2016-12-05 14:15:09 +0100

    Bug fixes.

--- setup.sh
+++ setup.sh
@@ -5,7 +5,6 @@ if [ $(pwd) = "/home/pi/WeLAB-4-makers" ]; then
     sudo apt-get -y install python-dev python-pip
     sudo pip install wiringpi
     git clone https://github.com/silvanmelchior/RPi_Cam_Web_Interface.git
-    sudo mv uconfig /var/www/html
     sudo mv php/getMediaSaved.php /var/www/html
     sudo mv microscope_led_server/ /opt
     sudo mv welab_4_makers /etc/init.d
